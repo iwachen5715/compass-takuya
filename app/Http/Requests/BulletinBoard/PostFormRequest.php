d051f570ff38b92d07ad45c49fca3f669ebe1eff
白井拓也 <>
2024-09-10 04:04:47 +0900

    “バリデーション確認用２”

--- app/Http/Requests/BulletinBoard/PostFormRequest.php
+++ app/Http/Requests/BulletinBoard/PostFormRequest.php
@@ -35,7 +35,7 @@ class PostFormRequest extends FormRequest
             'post_title.required' => 'タイトルは必ず入力してください。',
             'post_title.string' => 'タイトルは文字列型である必要があります。',
             'post_title.max' => 'タイトルは最大100文字までです。',
-            'post_body.required' => '投稿内容は必須項目です。',
+            'post_body.required' => '投稿内容は必ず入力してください。',
             'post_body.string' => '投稿内容は文字列型である必要があります。',
             'post_body.max' => '投稿内容は最大5000文字までです。',
             'post_category_id.required' => 'カテゴリーは必ず入力してください。',
